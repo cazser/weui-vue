cbb42b4e346c539dc90b38b686717c7ee86bc591
cazser <2264712072@qq.com>
2022-09-30 15:44:24 +0800

    +2

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,279 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import FoldPanel from './components/FoldPanel.vue';
+import LineItem from './components/LineItem.vue';
 
 </script>
 
 <template>
+  
   <div class="wrapper">
-  <h1>WeUI</h1>
-  <p>WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
-   <FoldPanel>
+    <div class="titleWrapper">
+      <h1>WeUI</h1>
+      <p>WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
+    </div>
+    <ul class="fold-list">
+      <li>
+    <FoldPanel>
     <template #header>
-    <h1>表单</h1>
+    <LineItem>
+      <template #header >
+        <span>表单</span>
+      </template>
+      <template #default >
+        <span>
+          <img src="./images/icon_nav_form.png" alt=" 展开"  class="small-img" />
+        </span>
+      </template>
+    </LineItem>
   </template>
 
   <template #default>
-    <p>A paragraph for the main content.</p>
-    <p>And another one.</p>
+    <ol class="list">
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>   
+    </ol>
   </template>
    </FoldPanel>
+ 
+   </li>
+     <li>
+    <FoldPanel>
+    <template #header>
+    <LineItem>
+      <template #header >
+        <span>表单</span>
+      </template>
+      <template #default >
+        <span>
+          <img src="./images/icon_nav_form.png" alt=" 展开"  class="small-img" />
+        </span>
+      </template>
+    </LineItem>
+  </template>
+
+  <template #default>
+    <ol class="list">
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>   
+    </ol>
+  </template>
+   </FoldPanel>
+ 
+   </li>
+     <li>
+    <FoldPanel>
+    <template #header>
+    <LineItem>
+      <template #header >
+        <span>表单</span>
+      </template>
+      <template #default >
+        <span>
+          <img src="./images/icon_nav_form.png" alt=" 展开"  class="small-img" />
+        </span>
+      </template>
+    </LineItem>
+  </template>
+
+  <template #default>
+    <ol class="list">
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>   
+    </ol>
+  </template>
+   </FoldPanel>
+ 
+   </li>
+     <li>
+    <FoldPanel>
+    <template #header>
+    <LineItem>
+      <template #header >
+        <span>表单</span>
+      </template>
+      <template #default >
+        <span>
+          <img src="./images/icon_nav_form.png" alt=" 展开"  class="small-img" />
+        </span>
+      </template>
+    </LineItem>
+  </template>
+
+  <template #default>
+    <ol class="list">
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>   
+    </ol>
+  </template>
+   </FoldPanel>
+ 
+   </li>
+     <li>
+    <FoldPanel>
+    <template #header>
+    <LineItem>
+      <template #header >
+        <span>表单</span>
+      </template>
+      <template #default >
+        <span>
+          <img src="./images/icon_nav_form.png" alt=" 展开"  class="small-img" />
+        </span>
+      </template>
+    </LineItem>
+  </template>
+
+  <template #default>
+    <ol class="list">
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>   
+    </ol>
+  </template>
+   </FoldPanel>
+ 
+   </li>
+     <li>
+    <FoldPanel>
+    <template #header>
+    <LineItem>
+      <template #header >
+        <span>表单</span>
+      </template>
+      <template #default >
+        <span>
+          <img src="./images/icon_nav_form.png" alt=" 展开"  class="small-img" />
+        </span>
+      </template>
+    </LineItem>
+  </template>
+
+  <template #default>
+    <ol class="list">
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>
+      <li>
+        <LineItem>
+          <template #header >
+            <span>Button</span>
+          </template>
+        </LineItem>
+      </li>   
+    </ol>
+  </template>
+   </FoldPanel>
+ 
+   </li>
+   </ul>
+
+
   </div>
 </template>
 
@@ -40,5 +296,22 @@ import FoldPanel from './components/FoldPanel.vue';
   background: #ededed;
   min-height: 100vh;
   padding: 20px;
+}
+
+.list>li{
+  border-bottom: 1px solid gray;
+
+}
+
+.list:last-child{
+  border-bottom: none;
+}
+
+.titleWrapper{
+  margin: 24px;
+}
+
+.fold-list>li{
+  margin: 8px;
 }
 </style>
--- src/components/LineItem.vue
+++ src/components/LineItem.vue
@@ -1,41 +1,25 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String
-})
 
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+ <div class="line-wrapper">
+   <slot name="header"></slot>
+   <slot></slot>
+ </div> 
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.line-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background: white;
+  padding: 12px;
 }
+
+
 </style>
 
