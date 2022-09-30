f0917df489106a4b4d03d2c8ffef6116413ad819
cazser <2264712072@qq.com>
2022-09-30 14:41:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+  <h1>WeUI</h1>
+  <p>WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.wrapper{
+  background: #ededed;
+  min-height: 100vh;
+  padding: 20px;
+}
+</style>
