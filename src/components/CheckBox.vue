990ff401c7a46905adf4cba0b95914db872cda38
cazser <2264712072@qq.com>
2022-10-01 13:21:46 +0800

    checkBox

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+   <span @click="click">checkbox</span>
+   <span> <slot></slot></span>
+</div>
+</template>
+
+<script setup>
+const click=()=>{
+  console.log("click");
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/CheckBoxView.vue
+++ src/views/CheckBoxView.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    复选框
+  <div class="checkbox-view-wrapper">
+    <h2>复选框样式展示</h2>
+    {{checked}}
+  <CheckBox v-model="checked">
+    standard is dealt for u.
+    </CheckBox>
+    <Button :prime="true" class="attachBottom">
+      下一步
+    </Button>
+
   </div>
 </template>
 
 <script setup>
-
+import { ref } from 'vue';
+import Button from '../components/Button.vue';
+import CheckBox from '../components/CheckBox.vue';
+const checked = ref(true);
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
+.checkbox-view-wrapper{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top:32px;
+  position: relative;
+}
 
+.attachBottom{
+  position: absolute;
+  top:calc( 100vh - 160px);
+}
 </style>
