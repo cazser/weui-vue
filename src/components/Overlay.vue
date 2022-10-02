f6dc0ebed94beac929dc0b387898b6c7dbbd0ba0
cazser <2264712072@qq.com>
2022-10-02 16:21:59 +0800

    增加 Overlay 组件

---
+++ src/components/Overlay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="weui-overlay"> 
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+.weui-overlay{
+  position:  fixed;
+  height: 100%;
+  width: 100%;
+  top:0;
+  left:0;
+  background-color: rgba(100, 100, 100, 0.5);
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
 import FoldPanel from '../components/FoldPanel.vue';
 import LineItem from '../components/LineItem.vue';
 import RightArrow from '../components/RightArrow.vue';
+import Overlay from '../components/Overlay.vue';
 
 </script>
 
@@ -14,11 +15,11 @@ import RightArrow from '../components/RightArrow.vue';
       <h1>WeUI</h1>
       <p>WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
     </div>
-    <div class="overlay">
+    <Overlay>
       <div class="content">
         content
       </div>
-    </div>
+    </Overlay>
     <ul class="fold-list">
       <li>
     <FoldPanel>
