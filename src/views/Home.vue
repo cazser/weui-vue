2c6da066382c1909d6f8325096e945cfc008dfa8
cazser <2264712072@qq.com>
2022-10-02 16:00:30 +0800

    test1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,11 @@ import RightArrow from '../components/RightArrow.vue';
       <h1>WeUI</h1>
       <p>WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
     </div>
+    <div class="overlay">
+      <div class="content">
+        content
+      </div>
+    </div>
     <ul class="fold-list">
       <li>
     <FoldPanel>
@@ -207,6 +212,24 @@ import RightArrow from '../components/RightArrow.vue';
 
 .fold-list>li{
   margin: 8px;
+}
+
+.overlay{
+  position:  fixed;
+  height: 100%;
+  width: 100%;
+  top:0;
+  left:0;
+  background-color: rgba(100, 100, 100, 0.5);
+}
+
+.content{
+  width: 100px;
+  height: 100px;
+  background:  red;
+  position: absolute;
+  top:100px;
+  left: 100px;
 }
 </style>
 
