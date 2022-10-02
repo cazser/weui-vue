0b24225e1c36ade88ecea8534f4d2cfe0ee45f54
cazser <2264712072@qq.com>
2022-10-02 16:28:49 +0800

    step1

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -2,20 +2,11 @@
   
   <template v-if="visible">
     
-    <div class="weui-dialog-overlay">
-    <div class="weui-dialog-wrapper">
-      
-        <header class="weui-dialog-title">{{title}}</header>
-        <main>
-          <slot></slot>
-        </main>
-        <footer>
-          <button>ok</button>
-          <button>cancel</button>
-        </footer>
-      
+  <overlay>
+    <div class="content">
+       content
     </div>
-    </div>
+  </overlay>    
     
   </template>
   
@@ -23,6 +14,7 @@
 <script>
 import Button from './Button.vue';
 import { Teleport } from 'vue';
+import Overlay from './Overlay.vue';
 export default {
   props: {
     visible: {
@@ -34,42 +26,17 @@ export default {
       default:""
     }
   },
-  components: {Button}
+  components: { Button, Overlay }
 }
 </script>
 
 <style scoped>
-  .weui-dialog{
-    border: none;
-    border-radius: 16px;
-     padding: 24px;
-     width: 270px;
-     z-index: 12;
-  }
-
-  .weui-dialog-wrapper{
-    position:  absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 12;
-  }
-  .weui-dialog-title{
-    display: flex;
-    padding-bottom: 24px;
-    justify-content:  center;
-    font-weight: bolder;
-
-  }
-
-  .weui-dialog-overlay{
-    border: 1px solid red;
-    background: rgba(102, 102, 102, 0.5);
-     position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 10;
-  }
+  .content{
+  width: 100px;
+  height: 100px;
+  background:  red;
+  position: absolute;
+  top:100px;
+  left: 100px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,7 @@ import LineItem from '../components/LineItem.vue';
 import RightArrow from '../components/RightArrow.vue';
 import Overlay from '../components/Overlay.vue';
 
+
 </script>
 
 <template>
@@ -15,7 +16,7 @@ import Overlay from '../components/Overlay.vue';
       <h1>WeUI</h1>
       <p>WeUI 是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。</p>
     </div>
-    
+   
     <ul class="fold-list">
       <li>
     <FoldPanel>
@@ -220,13 +221,6 @@ import Overlay from '../components/Overlay.vue';
   background-color: rgba(100, 100, 100, 0.5);
 }
 
-.content{
-  width: 100px;
-  height: 100px;
-  background:  red;
-  position: absolute;
-  top:100px;
-  left: 100px;
-}
+
 </style>
 
