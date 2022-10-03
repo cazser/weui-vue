24688a49a14b3bcf2a7a4fd37a59a6fc766f7006
cazser <2264712072@qq.com>
2022-10-03 09:02:07 +0800

    +1

--- src/components/SingleLevelPicker.vue
+++ src/components/SingleLevelPicker.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     Single Picker
+    {{visible}}
   </div>
 </template>
 
 <script>
   export default {
-    
+   props:{
+    visible: {type: Boolean, default: false}
+   } 
   }
 </script>
 
