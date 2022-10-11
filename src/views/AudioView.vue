c925e6a640924bb32972564df0755381ff345032
cazser <2264712072@qq.com>
2022-10-11 09:57:39 +0800

    +1

--- src/views/AudioView.vue
+++ src/views/AudioView.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-      aduio view
+  <div class="button-view-wrapper">
+    <div>AudioPlayer</div>
+    <div>音频播放器</div>
+    <div class="btn-list">
+      <AudioPlayer />
     </div>
-  <AudioPlayer></AudioPlayer>    
+  </div>
 </template>
 
 <script setup>
-import AudioPlayer from '../components/AudioPlayer.vue';
+  import Button from '../components/Button.vue';
+  import AudioPlayer from '../components/AudioPlayer.vue'
 </script>
 
-<style scoped lang="scss"></style>
+<style  scoped>
+.button-view-wrapper{
+  padding: 36px;
+  min-height: 100vh;
+  background-color: white;
+}
 
+.btn-item{
+  margin: 10px;
+}
+
+.btn-list{
+  width:100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
